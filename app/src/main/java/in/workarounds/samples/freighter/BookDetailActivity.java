9995ee4fa863167f60397732483d3e91fbd7523b
themadhukiran <pmadhukiran96@gmail.com>
2015-10-29 20:14:32 +0530

    update sample

---
+++ app/src/main/java/in/workarounds/samples/freighter/BookDetailActivity.java
@@ -0,0 +1,9 @@
+package in.workarounds.samples.freighter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by madki on 29/10/15.
+ */
+public class BookDetailActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/in/workarounds/samples/freighter/BookListActivity.java
@@ -0,0 +1,13 @@
+package in.workarounds.samples.freighter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by madki on 29/10/15.
+ */
+public class BookListActivity extends AppCompatActivity {
+    private String[] books = {"To kill a mocking bird", "Hitchhiker's guide to galaxy", "Catcher in the rye"};
+    private String[] authors = {"Harper Lee", "Douglas Adams", "J. D. Salinger"};
+
+    
+}
